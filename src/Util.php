40c1d7d537532de6e1e1a919eab5920a40afc731
johndoe29831 <johndoe29831@gmail.com>
2020-04-09 22:19:47 +0900

    utilに追加

--- src/Util.php
+++ src/Util.php
@@ -1,10 +1,56 @@
 <?php
+
 namespace Jd29;
 
 final class Util
 {
-    public static function hello () 
+    public static function hello()
     {
         echo "Hello World!";
+    }
+
+    /**
+     * python startsWith
+     *
+     * @param [type] $haystack
+     * @param [type] $needle
+     * @param boolean $case
+     * @return void
+     */
+    static public function startsWith($haystack, $needle, $case = true)
+    {
+        if ($case) {
+            return (strcmp(substr($haystack, 0, strlen($needle)), $needle) === 0);
+        }
+        return (strcasecmp(substr($haystack, 0, strlen($needle)), $needle) === 0);
+    }
+
+    /**
+     * python endsWith
+     *
+     * @param [type] $haystack
+     * @param [type] $needle
+     * @param boolean $case
+     * @return void
+     */
+    static public function endsWith($haystack, $needle, $case = true)
+    {
+        if ($case) {
+            return (strcmp(substr($haystack, strlen($haystack) - strlen($needle)), $needle) === 0);
+        }
+        return (strcasecmp(substr($haystack, strlen($haystack) - strlen($needle)), $needle) === 0);
+    }
+
+    /**
+     * 日時が正しいか検証
+     *
+     * @param [type] $date
+     * @param string $format
+     * @return void
+     */
+    static function validateDate($date, $format = 'Y-m-d H:i:s')
+    {
+        $d = \DateTime::createFromFormat($format, $date);
+        return $d && $d->format($format) == $date;
     }
 }
